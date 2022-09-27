ee84e8e94b9030dc3a1f5ff484a33959219bef66
HAREL <harel@gmail.com>
2022-09-27 22:36:41 +0300

    adding text

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+name = "Tom"
+age = 38
+tall = 6.8
+hobbie = "basketball"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("This name called", name, "and he is", age, "years old he is", tall, "feet height and he likes to play", hobbie, "he says this becuse he is", tall, "feet.")
